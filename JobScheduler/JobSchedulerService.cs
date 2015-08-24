c86bd6eb20e93523eb47a026daadef1cc2fefd9e
michaelsync <mchlsync@gmail.com>
2015-08-24 11:44:13 +1000

    added backend config reader

--- JobScheduler/JobSchedulerService.cs
+++ JobScheduler/JobSchedulerService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Akka.Actor;
+using Akka.DI.Ninject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +9,17 @@ using System.Threading.Tasks;
 namespace JobScheduler {
     public class JobSchedulerService {
         public void Start() {
+            InitDependencyInjection();
         }
+
+        private static void InitDependencyInjection() {
+            var container = new Ninject.StandardKernel();
+            //container.Bind<IWorkerService>().To(typeof(WorkerService));
+
+            var system = ActorSystem.Create("MyBackendProcessingSystem");
+            var propsResolver = new NinjectDependencyResolver(container, system);
+        }
+
         public void Stop() {
         }
     }
--- JobScheduler/Program.cs
+++ JobScheduler/Program.cs
@@ -1,39 +1,24 @@
-﻿using Akka.Actor;
-using Akka.DI.Ninject;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Topshelf;
+﻿using Topshelf;
 
 namespace JobScheduler {
     class Program {
         static void Main(string[] args) {
 
-            HostFactory.Run(x =>                                 //1
+            HostFactory.Run(x =>
             {
-                x.Service<JobSchedulerService>(s =>                        //2
+                x.Service<JobSchedulerService>(s =>
                 {
-                    s.ConstructUsing(name => new JobSchedulerService());     //3
-                    s.WhenStarted(tc => tc.Start());              //4
-                    s.WhenStopped(tc => tc.Stop());               //5
+                    s.ConstructUsing(name => new JobSchedulerService());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
                 });
-                x.RunAsLocalSystem();                            //6
+                x.RunAsLocalSystem();
 
-                x.SetDescription("Sample Topshelf Host");        //7
-                x.SetDisplayName("Stuff");                       //8
-                x.SetServiceName("Stuff");                       //9
+                x.SetDescription("This backend job service manager.");
+                x.SetDisplayName("JobScheduler");
+                x.SetServiceName("JobScheduler");
             });
-
-            // Create and build your container
-            var container = new Ninject.StandardKernel();
-            //container.Bind<TypedWorker>().To(typeof(TypedWorker));
-            //container.Bind<IWorkerService>().To(typeof(WorkerService));
-
-            // Create the ActorSystem and Dependency Resolver
-            var system = ActorSystem.Create("MySystem");
-            var propsResolver = new NinjectDependencyResolver(container, system);
+            
         }
     }
 }
