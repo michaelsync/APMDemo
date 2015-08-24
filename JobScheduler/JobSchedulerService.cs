d6c14dcc135afae013ec6e8f0e1c0290cb6ce638
michaelsync <mchlsync@gmail.com>
2015-08-24 12:40:41 +1000

    tried adding di but got fucked up with CA

--- JobScheduler/JobSchedulerService.cs
+++ JobScheduler/JobSchedulerService.cs
@@ -8,18 +8,22 @@ using System.Threading.Tasks;
 
 namespace JobScheduler {
     public class JobSchedulerService {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
         public void Start() {
             InitDependencyInjection();
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
         private static void InitDependencyInjection() {
-            var container = new Ninject.StandardKernel();
+            //var container = new Ninject.StandardKernel();
             //container.Bind<IWorkerService>().To(typeof(WorkerService));
 
-            var system = ActorSystem.Create("MyBackendProcessingSystem");
-            var propsResolver = new NinjectDependencyResolver(container, system);
+            //var system = ActorSystem.Create("MyBackendProcessingSystem");
+            //var propsResolver = new NinjectDependencyResolver(container, system);
+            //new NinjectDependencyResolver(container, system);
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
         public void Stop() {
         }
     }
